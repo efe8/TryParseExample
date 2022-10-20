f225ce1ed5fd485ccae539cea922f2e833c3a295
Efe Kaya <115823106+EfeKaya1@users.noreply.github.com>
2022-10-20 14:05:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace TryParse_örnekleri
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SayiAl("Sayı giriniz: ");
+
+        }
+        static int SayiAl(string mesaj)
+        {
+            int sayi;
+
+            while (true)
+            {
+                Console.Write(mesaj);
+                string giris = Console.ReadLine();
+
+                if (int.TryParse(giris, out sayi))
+                {
+                    return sayi;
+                }
+
+                Console.WriteLine("Hatalı giriş yapıldı.");
+
+            }
+        }
+
+    }
+}
+
+
+
+
+    
+
